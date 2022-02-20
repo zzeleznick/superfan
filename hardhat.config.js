639b72df595b32a40887779b79c89d166135115f
zzeleznick <zzeleznick@gmail.com>
2022-02-20 01:17:15 -0800

    wip: deploy to rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,29 @@ require("@nomiclabs/hardhat-web3");
 // testing the frontend.
 require("./tasks/faucet");
 
+// const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const MNEMONIC = process.env.MNEMONIC;
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 // If you are using MetaMask, be sure to change the chainId to 1337
 module.exports = {
   solidity: "0.8.6",
   networks: {
     hardhat: {
       chainId: 31337
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_KEY}`
+    },
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   }
 };
